7ee739bb952a10944482087925c4a3b9addad2b5
jtb2918 <jtb2918@ic.ac.uk>
2020-02-09 00:14:03 +0000

    Include error messages in ExerciseError

--- poser/app/src/main/java/org/ichack20/poser/exercises/ExerciseError.java
+++ poser/app/src/main/java/org/ichack20/poser/exercises/ExerciseError.java
@@ -1,5 +1,16 @@
 package org.ichack20.poser.exercises;
 
 public enum ExerciseError {
-  ELBOW_ERROR
+  ELBOW_ERROR("Keep your arms straight!");
+
+  private final String errorMessage;
+
+  ExerciseError(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public String toString() {
+    return errorMessage;
+  }
 }

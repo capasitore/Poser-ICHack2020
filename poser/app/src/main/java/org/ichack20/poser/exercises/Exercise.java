c44ea3d4cedfb8a602a5cab5ca61da6015f5aa60
jtb2918 <jtb2918@ic.ac.uk>
2020-02-09 00:13:45 +0000

    Make Exercise abstract

--- poser/app/src/main/java/org/ichack20/poser/exercises/Exercise.java
+++ poser/app/src/main/java/org/ichack20/poser/exercises/Exercise.java
@@ -1,8 +1,19 @@
 package org.ichack20.poser.exercises;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.ichack20.poser.Pose;
 
-public interface Exercise {
+public abstract class Exercise {
 
-  void update(Pose pose);
+  protected int reps;
+  protected Map<ExerciseError, Integer> errors;
+
+  public Exercise() {
+    this.reps = 0;
+    this.errors = new HashMap<>();
+  }
+
+  public abstract Orientation getOrientation();
+  public abstract void update(Pose pose);
 }
